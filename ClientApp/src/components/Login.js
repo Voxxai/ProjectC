74e253ab935661606e887a1b80eb993a37f63432
Henno85657 <85657@glr.nl>
2023-10-03 17:30:43 +0200

    Small testing on the project

---
+++ ClientApp/src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+  static displayName = Login.name;
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    );
+  }
+}
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import logo from '../images/Cavero_logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -25,18 +28,24 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">ProjectC</NavbarBrand>
+          <NavbarBrand tag={Link} to="/"><img className="Logo" src={logo}/></NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/">Week Overzicht</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/counter">Evenementen</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/fetch-data">Nieuws</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/fetch-data">Morgen</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to='/login'><FontAwesomeIcon icon={faUser} /></NavLink>
               </NavItem>
             </ul>
           </Collapse>
