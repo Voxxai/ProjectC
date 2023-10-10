9c4d04183ac807422d903ec722dfd5a61f1f6c06
DoubleKimp <karstenkeemink@gmail.com>
2023-10-10 13:15:53 +0200

    Evenementenpagina

---
+++ ProjectC-Client/src/pages/Evenementen.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+// import "./Login.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  } from '@fortawesome/free-solid-svg-icons'
+
+
+function Evenementen() {
+
+  return (
+    <div className=''>
+        
+    </div>
+  );
+}
+
+export default Evenementen;
