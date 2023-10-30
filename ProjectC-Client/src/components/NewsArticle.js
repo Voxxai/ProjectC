a7e1082d287263026852067b0b6b0cc8878aa0b8
Ward <113605493+Ward-Github@users.noreply.github.com>
2023-10-30 19:28:26 +0200

    temp

--- ProjectC-Client/src/components/NewsArticle.js
+++ ProjectC-Client/src/components/NewsArticle.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-function NewsArticle({ title, description }) {
+function NewsArticle({ title, description}) {
+  const truncatedDescription = description.length > 100
+    ? `${description.slice(0, 100)}...`
+    : description;
+
   return (
-    <div className="max-w-[400px] mx-3 mb-4">
+    <div className="max-w-[380px] mx-0 mb-2 border border-gray-300 rounded-lg p-2">
       <div className="bg-zinc-300 h-[182px]" />
-      <div className="text-fuchsia-800 text-3xl font-semibold mt-4">
+      <div className="text-cavero-purple text-2xl font-semibold mt-4">
         {title}
       </div>
       <div className="text-zinc-500 text-base mt-4">
-        {description}
+        {truncatedDescription}
       </div>
     </div>
   );
--- ProjectC-Client/src/pages/Nieuws.js
+++ ProjectC-Client/src/pages/Nieuws.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 
 function Nieuws() {
   const [news, setNews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const articlesPerPage = 6;
 
   useEffect(() => {
     const fetchNieuwsData = async () => {
@@ -19,19 +21,45 @@ function Nieuws() {
     fetchNieuwsData();
   }, []);
 
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticles = news.slice(indexOfFirstArticle, indexOfLastArticle);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(news.length / articlesPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
       <Topbar title="Nieuws" />
 
-      <div className="p-5 space-y-5 md:space-y-0 md:flex md:flex-wrap md:justify-between">
-        {news.map((article) => (
-          <div key={article.id} className="md:w-1/3 md:px-1">
+      <div className="p-3 space-y-5 md:space-y-0 md:flex md:flex-wrap md:justify-between">
+        {currentArticles.map((article) => (
+          <div key={article.id} className="md:w-1/3 md:px-0">
             <NewsArticle
               title={article.title}
               description={article.description}
               creation_time={article.creation_time}
             />
           </div>
+        ))}
+      </div>
+
+      <div className="flex justify-center mt-4">
+        {pageNumbers.map((number) => (
+          <button
+            key={number}
+            onClick={() => handlePageClick(number)}
+            className={`px-3 py-1 mx-2 ${currentPage === number ? 'bg-cavero-purple text-white' : 'bg-gray-200'
+              }`}
+          >
+            {number}
+          </button>
         ))}
       </div>
     </div>
