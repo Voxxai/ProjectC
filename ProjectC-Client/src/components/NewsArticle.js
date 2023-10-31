5113fd444cfc42184ca2c027038833538f91aa38
Henno0701 <henno.passchier@gmail.com>
2023-10-31 14:23:23 +0100

    Center articles

--- ProjectC-Client/src/components/NewsArticle.js
+++ ProjectC-Client/src/components/NewsArticle.js
@@ -6,7 +6,7 @@ function NewsArticle({ title, description}) {
     : description;
 
   return (
-    <div className="w-96 mx-10 mb-2 border border-gray-300 rounded-lg p-2 flex-1">
+    <div className="w-96 mb-2 border border-gray-300 rounded-lg p-2 flex-1">
       <div className="bg-zinc-300 h-[182px]" />
       <div className="text-cavero-purple text-2xl font-semibold mt-4">
         {title}
