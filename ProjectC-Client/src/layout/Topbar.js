40c1497dcaa7684c7be97de736b884e2e55e940a
Henno0701 <henno.passchier@gmail.com>
2023-10-31 13:49:50 +0100

    small fixes

--- ProjectC-Client/src/layout/Topbar.js
+++ ProjectC-Client/src/layout/Topbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import { Outlet, Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
