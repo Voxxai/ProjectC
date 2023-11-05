3291f66b7fd6687b4289f1d6a1f5cf7b42767add
Henno0701 <henno.passchier@gmail.com>
2023-11-05 22:50:14 +0100

    personen dropdown

--- ProjectC-Client/src/pages/WeekOverzicht.js
+++ ProjectC-Client/src/pages/WeekOverzicht.js
@@ -1,17 +1,19 @@
 // import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronUp, faCircle, faCircleUser, faUserAlt } from '@fortawesome/free-solid-svg-icons'
 import axios, { all } from 'axios';
 
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 
 function WeekOverzicht() {
+    const usersDropdown = useRef(null)
     const [ dates, setDates ] = useState([]);
+    const [ open, setOpen ] = useState(false);
     const [ error, setError ] = useState(false);
     const [ events, setEvents ] = useState([]);
-const [ users, setUsers ] = useState([]);
+    const [ users, setUsers ] = useState([]);
 
 // Onload set dates of this week
 useEffect(() => {
@@ -103,26 +105,36 @@ useEffect(() => {
                 {/* Items */}
                 {dates.map((Date, index) => (
                     <div className="bg-white rounded-md text-gray-400 w-3/12 p-2 shadow-md">
+                        <div className='relative'>
                         <span className='font-medium text-lg'>{Date.Week}</span>
                         <div className="flex flex-row text-black items-end gap-1.5">
                             <span className="text-slate-700 text-3xl font-medium">{Date.Day}</span>
                             <span className='text-slate-700 text-lg font-semibold'>{Date.Month}</span>
                         </div>
 
-                        {/* Items Users */}
+                        {/* Users button */}
                         <div className='flex flex-row'>
+                            <button ref={usersDropdown} className='bg-cavero-purple text-white w-full p-1 text-left rounded-sm mt-2' onClick={() => setOpen(!open)}>Personen
+                            <FontAwesomeIcon icon={ open ? faChevronUp : faChevronDown } className=' float-right mt-1 ml-1 fa-sm'/>
+                            </button>
+                        </div>
+                        
+                        {/* Dropdown Items Users */}
+                        <div className={`${ open ? "opacity-100" : "opacity-0" }  duration-150 flex flex-col bg-white w-full p-2.5 shadow-md rounded-sm absolute`}>
+                            <p className='w-full'>Deze werknemers zijn aanwezig.</p>
                             {dates[index].Users.map((user, index) => (
-                                 <div className='flex flex-col'>
-                                    <span className='text-slate-700 text-sm font-semibold'>{user.Account_ID}</span>
-                                     <span className='text-slate-700 text-sm font-semibold'>{user.Date}</span>
+                                 <div className='flex flex-row bg-cavero-purple-light w-full p-2 rounded-md place-items-center gap-x-2 mb-1'>
+                                    <FontAwesomeIcon className='text-cavero-purple fa-2x' icon={faCircleUser}/>
+                                    <span className='text-slate-700 text-sm font-semibold'>{user.Voornaam} {user.Achternaam}</span>
                                  </div>
+                                 
                             ))}
-
+                                 
                         </div>
 
-                        {/* events card */}
+                        {/* Events Card */}
                         {dates[index].Events.map((event, index2) => (
-                            <div className='flex flex-row bg-cavero-purple-light rounded-md p-2 my-1'>
+                            <div className='flex flex-row bg-cavero-purple-light rounded-md p-2 my-1 font-semibold'>
                                 {/* {console.log(event.Titel)} */}
                                     <div className='flex flex-row place-items-center gap-x-2'>
                                         <div className='w-2.5 h-2.5 bg-cavero-purple rounded-full'></div>
@@ -134,10 +146,12 @@ useEffect(() => {
                             </div>
                         ))}
                     </div>
+                    </div>
                 ))}
 
             </div>
         </div>
+    
     );
 }
 
