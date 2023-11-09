fb6081a71d7599356d7ca658f8a8e7c4dc5c620d
Henno0701 <henno.passchier@gmail.com>
2023-11-09 11:38:47 +0100

    Profiel page added

--- ProjectC-Client/src/App.js
+++ ProjectC-Client/src/App.js
@@ -6,6 +6,7 @@ import Layout from './layout/Layout';
 import WeekOverzicht from "./pages/WeekOverzicht";
 import RequireAuth from './components/RequireAuth';
 import Nieuws from './pages/Nieuws';
+import Profiel from './pages/Profiel';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path='/Evenementen' element={<WeekOverzicht />} />
           <Route path='/Nieuws' element={<Nieuws />} />
           <Route path='/Morgen' element={<WeekOverzicht />} />
+          <Route path='/Profiel' element={<Profiel />} />
          </Route> 
       </Route>
     </Routes>
