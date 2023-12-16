0173f9678ab66a26950697720f361122063dd589
Voxxai <gilkranendonk@gmail.com>
2023-12-16 13:35:37 +0100

    Nieuws pagina en full article wou op samsung telefoons nog zijwaarts scrollen

--- ProjectC-Client/src/components/NewsArticleFull.js
+++ ProjectC-Client/src/components/NewsArticleFull.js
@@ -5,13 +5,13 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 function NewsArticleFull({ title, description, onBackClick, creation_time }) {
 
     return (
-        <div className="w-[800px]">
+        <div className="w-[800px] max-sm:w-[325px]">
             <button onClick={onBackClick} className=" text-cavero-purple text-md font-medium">
             <FontAwesomeIcon icon={faArrowLeft} className="mr-1" />
                 Back
             </button>
           <div className="bg-zinc-300 h-[250px]" />
-          <div className="text-cavero-purple text-3xl font-semibold mt-4">
+          <div className="text-cavero-purple text-3xl font-semibold mt-4 max-sm:text-2xl">
             {title}
           </div>
           <div className="text-zinc-500 text-base mt-4">
