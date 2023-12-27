d0884037ee71865cbd931218b1e3a600e3f7671a
Voxxai <gilkranendonk@gmail.com>
2023-12-27 13:49:31 +0100

    Registreer pagina is gemaakt en werkt ook met sha1 hashing

--- ProjectC-Client/src/pages/Login.js
+++ ProjectC-Client/src/pages/Login.js
@@ -304,7 +304,7 @@ function Login() {
                         <div className='input-container mb-4'>
                             <div className='flex relative items-center'>
                                 <FontAwesomeIcon icon={faEnvelope} color='black' className='absolute w-6 h-6 text-gray-500 p-1' />
-                                <input type="email" className='w-full h-12 pl-9 border-none outline-none rounded shadow-none' name='email' id="email" placeholder="E-mailadres" onChange={handleInput} required />
+                                <input type="email" className='w-full h-12 pl-9 border-none outline-none rounded shadow-none ml-6' name='email' id="email" placeholder="E-mailadres" onChange={handleInput} required />
                             </div>
                             <div className='h-[3px] bg-cavero-purple duration-300 rounded-full ColoredLine'></div>
                         </div>
@@ -312,12 +312,16 @@ function Login() {
                         <div className='input-container mb-4'>
                             <div className='flex relative items-center'>
                                 <FontAwesomeIcon icon={faLock} color='black' className='absolute w-6 h-6 text-gray-500 p-1' />
-                                <input type="password" className='w-full h-12 pl-9 border-none outline-none rounded shadow-none' name='password' id="password" placeholder="Wachtwoord" onChange={handleInput} required />
+                                <input type="password" className='w-full h-12 pl-9 border-none outline-none rounded shadow-none ml-6' name='password' id="password" placeholder="Wachtwoord" onChange={handleInput} required />
                             </div>
                             <div className='h-[3px] bg-cavero-purple duration-300 rounded-full ColoredLine'></div>
                         </div>
                         <div className='flex flex-row items-center mb-3'>
-                            <a className='flex-1'>Wachtwoord vergeten?</a>
+                            <div className='flex-col'>
+                                <a href='/register' className='flex-1'>Nog geen account?</a> <br />
+                                <a className='flex-1'>Wachtwoord vergeten?</a>
+                            </div>
+                            <div className='flex-1'></div>
                             <button type="button" className='bg-gradient-to-r from-cavero-purple to-[#c279cc] text-white w-32 h-9 rounded-full duration-300 hover:scale-105 hover:shadow-lg' onClick={getUser}>Login <FontAwesomeIcon icon={faArrowRightLong} color='white' /></button>
                         </div>
 
