7b7e8001b9a5719adc08ef56fb9f958fe445fcf1
Ward <113605493+Ward-Github@users.noreply.github.com>
2024-01-15 17:56:23 +0200

    Update NewsArticleFull.js

--- ProjectC-Client/src/components/NewsArticleFull.js
+++ ProjectC-Client/src/components/NewsArticleFull.js
@@ -13,7 +13,7 @@ function NewsArticleFull({ id, title, description, onBackClick, creation_time, i
   };
 
   return (
-    <div className="max-md:w-full w-1/2 relative">
+    <div className="w-[640px] relative max-md:w-full">
       <div className='flex flex-row justify-between items-center overflow-y-auto'>
         <button
           onClick={onBackClick}
