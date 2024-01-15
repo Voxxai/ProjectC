3ae13ebccd9d7799f3a846642aa07f15697767a2
Henno0701 <henno.passchier@gmail.com>
2024-01-15 18:13:07 +0100

    Thema disabled

--- ProjectC-Client/src/pages/Settings.js
+++ ProjectC-Client/src/pages/Settings.js
@@ -13,11 +13,11 @@ function Settings() {
 
         <div className='flex-col'>
             <span className='font-semibold text-lg'>Thema</span>
-            <p className='text-md text-gray-500'>Kies een thema aan die u graag wilt hebben over de applicatie heen.</p>
+            {/* <p className='text-md text-gray-500'>Kies een thema aan die u graag wilt hebben over de applicatie heen.</p>
             <select>
                 <option value="light">Licht</option>
                 <option value="dark">Donker</option>
-            </select>
+            </select> */}
         </div>
 
         {/* <button className='flex flex-row gap-x-2 items-center justify-center w-full h-10 bg-cavero-purple rounded-md text-white hover:bg-cavero-purple-dark duration-200 scale-95' onClick={sendEmail()}>Klik me</button> */}
